1942648228864ed8d645305d97003f0b2e4a573b
angelpluz <t.phomprasert@gmail.com>
2023-11-20 17:53:56 +0700

    asdasdas

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,22 +2,14 @@ import React, { useState, useEffect } from 'react';
 import Header from '../components/Header';
 import styles from '../styles/Home.module.css';
 
-
-interface Show {
-  name: string;
-  type: string;
-  language: string;
-  genres: string[];
-  // ระบุคุณสมบัติอื่นๆ ที่มีใน API
-}
 const Home = () => {
-  const [show, setShow] = useState<Show | null>(null);
+  const [apiResponse, setApiResponse] = useState(null);
+
   useEffect(() => {
-    fetch('https://api.tvmaze.com/search/shows?q=golden')
+    fetch('https://toyotathailand4u.com/salemaninformation/109A1')
       .then(response => response.json())
       .then(data => {
-        // สมมติว่าเราสนใจเฉพาะผลลัพธ์แรก
-        setShow(data[0].show);
+        setApiResponse(data);
       })
       .catch(error => console.error('Error fetching data:', error));
   }, []);
@@ -25,24 +17,13 @@ const Home = () => {
   return (
     <div className={styles.container}>
       <Header />
-      {show ? (
+      {apiResponse ? (
         <div>
-          <h1>{show.name}</h1>
-          <p>Type: {show.type}</p>
-          <p>Language: {show.language}</p>
-          <p>Genres: {show.genres.join(', ')}</p>
-          <p>Status: {show.status}</p>
-          <p>Runtime: {show.runtime} minutes</p>
-          <p>Premiered: {show.premiered}</p>
-          <p>Ended: {show.ended}</p>
-          <p>Network: {show.network.name}</p>
-          <p>Country: {show.network.country.name}</p>
-          <p><a href={show.url}>Official Site</a></p>
-          <img src={show.image.medium} alt={`Cover for ${show.name}`} />
-          <div dangerouslySetInnerHTML={{ __html: show.summary }}></div>
+          <h1>API Response</h1>
+          <h1>API Response</h1>
         </div>
       ) : (
-        <p>Loading...</p>
+        <p>กำลังโหลดข้อมูล...</p>
       )}
     </div>
   );
