d32cfaa3b6987c3e720f30a16f558418d81ad0d4
angelpluz <t.phomprasert@gmail.com>
2023-11-20 17:13:15 +0700

    test04

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,55 +1,31 @@
 import React, { useState, useEffect } from 'react';
-import Header from '../components/Header'; // Make sure this path is correct
-import styles from '../styles/Home.module.css'; // Make sure this path is correct
+import Header from '../components/Header';
+import styles from '../styles/Home.module.css';
 
-
-interface SalesmanData {
-  name: string;
-  nickname: string;
-  sale_code: string;
-  // ระบุคุณสมบัติอื่นๆ ตามที่อยู่ในข้อมูลของคุณ
-}
 const Home = () => {
-  const [salesmanData, setSalesmanData] = useState<SalesmanData | null>(null);
+  const [shows, setShows] = useState([]);
 
   useEffect(() => {
-    const fetchSalesmanData = async () => {
-      try {
-        const response = await fetch('http://toyotathonburi.co.th/api/salesmandata/search/109A1M');
-        const data = await response.json();
-        if (data.status) {
-          console.log('Data fetched successfully:', data.data);
-          setSalesmanData(data.data);
-        } else {
-          console.log('Data fetch status was false');
-        }
-      } catch (error) {
-        console.error('Error fetching data:', error);
-      }
+    const fetchShows = async () => {
+      const response = await fetch('https://api.tvmaze.com/search/shows?q=golden');
+      const data = await response.json();
+      setShows(data);
     };
 
-    fetchSalesmanData();
+    fetchShows();
   }, []);
 
   return (
     <>
-      <Header /> {/* Header is now outside of the main container */}
+      <Header />
       <div className={styles.container}>
-        <h1 className={styles.title}>ยินดีต้อนรับสู่เว็บไซต์ของฉัน</h1>
-        {salesmanData.length > 0 ? (
-          <div>
-            {salesmanData.map((item, index) => (
-              <div key={index}>
-                <h2>{item.name} ({item.nickname})</h2>
-                <p>Code: {item.sale_code}</p>
-                {/* Include additional data display here */}
-              </div>
-            ))}
+        <h1 className={styles.title}>TV Shows</h1>
+        {shows.map((show, index) => (
+          <div key={index}>
+            <h2>{show.show.name}</h2>
+            {/* แสดงข้อมูลอื่นๆ ของรายการทีวีที่นี่ */}
           </div>
-        ) : (
-          <p>กำลังโหลดข้อมูล...</p>
-        )}
-        {/* More content here */}
+        ))}
       </div>
     </>
   );
