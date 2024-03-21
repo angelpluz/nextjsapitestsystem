0d9afcb0b75201edc3018358695572d3ce22ad06
angelpluz <t.phomprasert@gmail.com>
2024-03-21 11:46:19 +0700

    update

--- src/components/CookiePopup.tsx
+++ src/components/CookiePopup.tsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import styles from '../styles/CookiePopup.module.css'; // Adjust the path to your CSS file accordingly
+import Link from 'next/link';
 
 const CookiePopup = () => {
   const [isVisible, setIsVisible] = useState(true);
@@ -16,9 +17,10 @@ const CookiePopup = () => {
       <p className={styles.message}>
       เราใช้คุกกี้เพื่อประสบการณ์ใช้งานที่ดีและตรงตามความต้องการของลูกค้า โดยการใช้เว็บไซต์ของเราคุณยินยอมให้เราใช้คุกกี้ตามนโยบายความเป็นส่วนตัวของเรา
       </p>
-      <p className={styles.message}>
-      ข้อมูลเพิ่มเติม
-      </p>
+      <Link href="/PolicyPage" passHref>
+      <p className={styles.navLabel}>ข้อมูลเพิ่มเติม</p>
+          
+      </Link>
       <button onClick={handleClose} className={styles.closeButton}>
         ยอมรับคุกกี้
       </button>
